0abda1af6e1a123b16d97c59082ff2ebeef34caa
Thomas Willumsen <mail@thomasw.dk>
2023-09-30 19:56:45 +0200

    remove datacontract from request models

--- src/Serilog.Sinks.Humio/HumioBatch.cs
+++ src/Serilog.Sinks.Humio/HumioBatch.cs
@@ -1,26 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace Serilog.Sinks.Humio
 {
-    [DataContract]
     internal class HumioBatch
     {
-        [DataMember(Name = "tags")]
         public dynamic Tags { get; set; }
 
-        [DataMember(Name = "events")]
         public IEnumerable<HumioLogEvent> Events { get; set; }
     }
 
-    [DataContract]
     internal class HumioLogEvent
     {
-        [DataMember(Name = "timestamp")]
         public DateTimeOffset Timestamp { get; set; }
 
-        [DataMember(Name = "attributes")]
         public RawJson Attributes { get; set; }
     }
 }
